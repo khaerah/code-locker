db52e8c4b726eb3331528b6c4b4779f349f25c20
khairah <khaeraholamide@gmail.com>
2024-09-08 21:59:25 +0100

    first commit

---
+++ puzzlify.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+});
